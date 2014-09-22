97f910b8ce302568a782c69a46ae8f483ac59bcb
AJ Ostrow <alexander.ostrow@gmail.com>
2014-09-22 19:21:31 -0400

    fixed firefox

--- index.js
+++ index.js
@@ -1,69 +1,87 @@
 $(function () {
+
+  // constants
+
   var UP = 38
   var DOWN = 40
   var LEFT  = 39
   var RIGHT = 37
   var ESCAPE = 27
 
+  // globals
+
   var $container = $('#container')
   var $background = $('#background')
   var $label = $('#metro')
   var $map = $('#map')
+  var $copyright = $('#copyright')
   var $paper = null
   var $line = null
   var $station = null
   var $history = []
+  var $preload = []
 
-  init()
+  // ui handlers
 
-  function init () {
-    resize()
-    $(window).resize($.debounce(500, resize))
+  $(window).resize($.debounce(500, resize))
 
-    $('.stm-line').on('click', function (ev) {
-      ev.stopPropagation()
-      var data = ev.target.dataset
-      $line = data.line
-      navigate(window.stm[data.station])
-    })
+  $(document).keydown($.debounce(300, true, keydown))
 
-    $(document).keydown(function (ev) {
-      var key = ev.which
-      if (key == UP) {
-        next('y', -1)
-      } else if (key == DOWN) {
-        next('y', 1)
-      } else if (key == LEFT) {
-        next('x', 1)
-      } else if (key == RIGHT) {
-        next('x', -1)
-      } else if (key == ESCAPE) {
-        map(true)
-      }
-      $('#copyright').hide()
-    })
+  $background.on('click', function (ev) {
+    map(true)
+  })
 
-    $background.on('click', function (ev) {
+  $background.on('contextmenu', function (ev) {
+    ev.preventDefault()
+    $copyright.show()
+  })
+
+  $container.on('click', function (ev) {
+    if (!$station) { navigate() }
+    else { map(false) }
+  })
+
+  // click ride a line
+  $('.stm-line').on('click', function (ev) {
+    ev.stopPropagation()
+    var target = $(ev.target)
+    $line = target.attr('data-line')
+    var id = target.attr('data-station')
+    navigate(window.stm[id])
+  })
+
+  // click a station
+  $map.on('click', '.stm-target', function (ev) {
+    ev.stopPropagation()
+    var id = $(ev.target).attr('data-id')
+    render(window.stm[id])
+    map(false)
+  })
+
+  // init
+
+  resize()
+
+  // lib
+
+  function keydown (ev) {
+    switch (ev.which) {
+    case UP:
+      next('y', -1)
+      break
+    case DOWN:
+      next('y', 1)
+      break
+    case LEFT:
+      next('x', 1)
+      break
+    case RIGHT:
+      next('x', -1)
+      break
+    case ESCAPE:
       map(true)
-      $('#copyright').hide()
-    })
-
-    $background.on('contextmenu', function (ev) {
-      ev.preventDefault()
-      $('#copyright').show()
-    })
-
-    $container.on('click', function (ev) {
-      if (!$station) { navigate() }
-      else { map(false) }
-    })
-
-    $map.on('click', '.stm-target', function (ev) {
-      ev.stopPropagation()
-      render(window.stm[ev.target.dataset.id])
-      map(false)
-      $('#copyright').hide()
-    })
+      break
+    }
   }
 
   function next (property, sign) {
@@ -96,82 +114,92 @@ $(function () {
       station = window.stm[ids[index]]
     }
     map(true)
-    render(station) 
-    var target = station.target.node
-    target.setAttribute('class', 'stm-target active')   
+    $(station.target.node).attr('class', 'stm-target active')
+    render(station)
     setTimeout(function () {
-      target.setAttribute('class', 'stm-target')
+      $(station.target.node).attr('class', 'stm-target')
       map(false)
     }, 800)
   }
 
   function resize () {
-    // set map height
+    // the ratio 800x2017 is from metro-map.png
+    // magic number 1.2 determined experimentally
     var height = $(window).height() * 1.2
     var width = height * (800 / 1027) * 1.2
     $map.width(width).height(height)
 
-    // draw points
+    // remove old canvase
+    if ($paper) { $paper.remove() }
+
+    // draw new points
     $paper = Raphael($map[0], width, height)
-    $('circle', $map).remove()
-    Object.keys(window.stm).forEach(function (id) {
-      plot(window.stm[id])
-    })
-    tooltip()
+    _.each(window.stm, plot)
+    tooltip(true)
   }
 
   function plot (station) {
     // pct to pixels
-    var point = toPixels(station)
+    var point = {
+      x: station.x * $map.width(),
+      y: station.y * $map.height(),
+    }
     station.point = point
 
     // draw visible point
     var circle = $paper.circle(point.x, point.y, 4)
-    circle.attr('fill', '#eee')
-    circle.attr('stroke', '#eee')
+    circle.attr('fill', '#ddd')
+    circle.attr('stroke', '#ddd')
     station.circle = circle
 
     // draw invisible larger click area
     var target = $paper.circle(point.x, point.y, 10)
-    target.node.dataset.id = station.id
-    target.node.setAttribute('class', 'stm-target')
+    $(target.node).attr({
+      'class': 'stm-target',
+      'data-id': station.id,
+    })
     station.target = target
   }
 
   function render (station) {
-    remember(station)
+    // history
+    if (!$line || !station.lines[$line]) {
+      $line = _.first(_.keys(station.lines))
+    }
+    $history.push($station)
+    $station = station
+
+    // visible change
     $label.text(station.id)
     $background.show().css({
       'background-image': 'url("' + station.url + '")',
     })
     station.circle.attr('fill', station.color)
     station.circle.attr('stroke', station.color)
+
+    // preload next stations
+    var line = station.lines[$line]
+    var ids = _.compact([ line.next, line.prev ])
+    _.each(ids, function (id) {
+      $.get(window.stm[id].url)
+    })
   }
 
-  function remember (station) {
-    if (!$line || !station.lines[$line]) {
-      $line = Object.keys(station.lines)[0]
-    } 
-    $history.push($station)
-    $station = station
-  }
-
-  function tooltip () {
-    $('.stm-target').tipsy({ gravity: 's', fade: true, title: 'data-id' })
-  }
-
-  function map (visible) {
-    if (visible) {
-      return $container.fadeIn()
+  function tooltip (yes) {
+    if (yes) {
+      $('.stm-target').tipsy({ gravity: 's', fade: true, title: 'data-id' })
     } else {
       $('.tipsy').remove()
-      return $container.fadeOut()
     }
   }
 
-  function toPixels (station) {
-    var width = $map.width()
-    var height = $map.height()
-    return { x: station.x * width, y: station.y * height }
+  function map (yes) {
+    if (yes) {
+      $copyright.hide()
+      $container.fadeIn()
+    } else {
+      tooltip(false)
+      $container.fadeOut()
+    }
   }
 })
